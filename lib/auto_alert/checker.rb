6cba551d75553217c7f5821099b38b117b3b8155
Brendan Tang <b@brendantang.net>
2021-06-15 13:39:25 -0400

    Refactor Checker#check to use case pattern matching

--- lib/auto_alert/checker.rb
+++ lib/auto_alert/checker.rb
@@ -2,31 +2,50 @@ module AutoAlert
   class Checker
     attr_reader :kind
 
-    def initialize(alert_kind, raise_condition, dismiss_condition, message_builder)
+    def initialize(alert_kind, raise_condition, dismiss_condition, reraise_condition = false, message_builder)
       @kind = alert_kind
       @raise_condition = normalize_check_condition raise_condition
       @dismiss_condition =
         dismiss_condition ?
           normalize_check_condition(dismiss_condition) :
           ->(a) { not @raise_condition.call(a) }
+      @reraise_condition = if !reraise_condition # If nil or false
+          nil
+        elsif reraise_condition == true
+          @raise_condition
+        else
+          normalize_check_condition reraise_condition
+        end
       @message_builder = message_builder
     end
 
     def check(alertable)
       existing_alert = alertable.send("#{@kind}_alert")
 
-      # Don't re-raise alerts that have already resolved.
-      return if existing_alert&.resolved
+      case [existing_alert, @reraise_condition, existing_alert&.resolved]
 
-      if existing_alert && @dismiss_condition.call(alertable)
-        # Dismiss existing alert
-        existing_alert.update(resolved: true)
-      elsif existing_alert
-        # Update message if alert still applies
-        existing_alert.update(message: message(alertable))
-      elsif @raise_condition.call(alertable)
-        # Raise alert for the first time
-        alertable.alerts.create(kind: @kind, message: message(alertable), resolved: false)
+      # Check for alert for the first time
+      in nil, _, _
+        if @raise_condition.call(alertable)
+          alertable.alerts.create(kind: @kind, message: message(alertable), resolved: false)
+        end
+
+      # Check if alert should be dismissed
+      in alert, _, false
+        alert.update(resolved: true) if @dismiss_condition.call(alertable)
+
+      # Resolved alert should not be re-raised
+      in alert, nil, true
+        return
+
+      # Check if resolved alert should be re-raised
+      in alert, reraise_condition, true
+        if reraise_condition.call(alertable)
+          alert.update(message: message(alertable), resolved: false)
+        end
+
+      else
+        return
       end
     end
 
